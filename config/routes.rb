fa71280d31b4cd289cd38bfdea0dce38bff2fa61
Mike Barile <mikebarile13@gmail.com>
2016-10-28 14:08:39 -0700

    Set up entry file and successfully rendered to browser

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :static_pages, only: [:show]
+  root to: 'static_pages#show'
 end
