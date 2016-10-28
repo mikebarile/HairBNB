a6389b3298b8008a1b5b4fe2dc8242a86fecc82d
Mike Barile <mikebarile13@gmail.com>
2016-10-28 15:29:54 -0700

    Developed and tested frontend auth util functions

--- frontend/hair_bnb.jsx
+++ frontend/hair_bnb.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Imports for testing
+import {signup, login, logout} from './util/session_api_util';
+//
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to HairBnB!</h1>, root);
+
+    // Testing window functions
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+    //
 });
