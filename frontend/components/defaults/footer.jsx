30dfef1dfc0e937a5c4fc7ef94ed8d35dd609594
Mike Barile <mikebarile13@gmail.com>
2016-11-02 18:01:14 -0700

    Fixed fonts bug

--- frontend/components/defaults/footer.jsx
+++ frontend/components/defaults/footer.jsx
@@ -7,7 +7,8 @@ const Footer = (props) => {
     <div className="footer">
       <span className="footer-text-container">
         <a className="footer-text" href="https://www.linkedin.com/in/mikebarile">Company</a>
-        <a className="footer-text" href="https://github.com/mikebarile/HairBNB">Discover</a>
+        <a className="footer-text" href="https://github.com/mikebarile/HairBNB">About</a>
+        <a className="footer-text" href="https://www.mikebarile.com">Discover</a>
         <a className="footer-text" href="https://hairbnb13.herokuapp.com/">Hosting</a>
       </span>
     </div>
