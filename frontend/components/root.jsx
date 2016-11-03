c1a7196852176445ef644edea98536879fa1411c
Mike Barile <mikebarile13@gmail.com>
2016-11-03 16:12:55 -0700

    Fixed css styling on top bar

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,7 +4,7 @@ import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-rou
 import App from './app.jsx';
 import SessionFormContainer from './session_form/session_form_container';
 import UserFormContainer from './user_form/user_form_container';
-import ListingContainer from './listing-show/listing_container';
+// import ListingContainer from './listing-show/listing_container';
 import Home from './home/home';
 
 const Root = ({ store }) => {
@@ -16,13 +16,15 @@ const Root = ({ store }) => {
     }
   };
 
+  // <Route path="listings/:listingId" component={ListingContainer}/>
+
   return (
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/" component={App} >
         <IndexRedirect to="/home" />
         <Route path="/home" component={Home}/>
-        // <Route path="listings/:listingId" component={ListingContainer}/>
+
       </Route>
     </Router>
   </Provider>
