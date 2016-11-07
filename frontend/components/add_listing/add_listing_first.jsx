63800ed17496f62f7eaa6975dd7c3812155cff8d
Mike Barile <mikebarile13@gmail.com>
2016-11-07 10:27:54 -0800

    Built out first listing form; made text boxes automatically resize and gave them character limit spans

--- frontend/components/add_listing/add_listing_first.jsx
+++ frontend/components/add_listing/add_listing_first.jsx
@@ -4,6 +4,12 @@ import { Link } from 'react-router';
 class AddListingFirst extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      "title": 50,
+      "description": 500
+    };
+    this.textUpdate = this.textUpdate.bind(this);
+    this.handleCounter = this.handleCounter.bind(this);
   }
 
   componentDidMount() {
@@ -16,11 +22,58 @@ class AddListingFirst extends React.Component {
     }
   }
 
+  textUpdate(field, max) {
+    return (e) => {
+      e.target.style.height = "0px";
+      e.target.style.height = (e.target.scrollHeight + 1)+"px";
+      this.setState({[field]: max - e.target.value.length});
+    };
+  }
+
+  handleCounter(field, min) {
+    if (this.state[field] <= min) {
+      return `alf-${field}-counter alf-low`;
+    }
+    else {
+      return `alf-${field}-counter`;
+    }
+  }
+
   render() {
     let user = this.props.currentUser;
     return (
       <div className="add-listing-form">
-        TEST
+        <div className="alf-first-half">
+          <div className="alf-col-first">
+            <span className="alf-field-title">Name your place</span>
+            <div className="alf-input-row">
+              <textarea
+                className="alf-text-full"
+                onKeyUp={this.textUpdate("title", 50)}>
+              </textarea>
+              <span className={this.handleCounter("title", 10)}>{this.state.title}</span>
+            </div>
+
+            <span className="alf-field-title">Edit your description</span>
+              <div className="alf-input-row">
+                <textarea
+                  className="alf-text-full"
+                  onKeyUp={this.textUpdate("description", 500)}>
+                </textarea>
+                <span className={this.handleCounter("description", 50)}>{this.state.description}</span>
+              </div>
+
+            <span className="alf-field-title">Upload a photo</span>
+            <input type="file" className="alf-file"/>
+          </div>
+        </div>
+
+        <div className="alf-second-half">
+          <div className="alf-col-second">
+            <div className="alf-tip-box">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
