a33cbd497e41f3a2fe80e3dce330674ed7171f4f
Mike Barile <mikebarile13@gmail.com>
2016-11-07 17:00:15 -0800

    Updated buttons for form

--- frontend/components/add_listing/add_listing_second.jsx
+++ frontend/components/add_listing/add_listing_second.jsx
@@ -4,9 +4,6 @@ import { Link, withRouter } from 'react-router';
 class AddListingSecond extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
     this.handleNext = this.handleNext.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
   }
@@ -30,6 +27,24 @@ class AddListingSecond extends React.Component {
     this.props.router.push('/become-a-host/ready');
   }
 
+  handleButton(field) {
+    let status = this.props.listingFormState[field];
+    if (status) {
+      return `alf-amenity-true alf-${field}`;
+    }
+    else {
+      return `alf-amenity-false`;
+    }
+  }
+
+  handleButtonClick(field) {
+    return (e) => {
+      e.preventDefault();
+      let truthy = !(this.props.listingFormState[field])
+      this.props.updateListingForm({[field]: truthy})
+    }
+  }
+
   render() {
     let user = this.props.currentUser;
     return (
@@ -37,16 +52,44 @@ class AddListingSecond extends React.Component {
         <div className="alf-first-half">
           <div className="alf-col-first">
             <div className="alf-form">
-              <span className="alf-field-title">Name your place</span>
+              <span className="alf-field-title">Add amenities</span>
+              <div className="alf-input-buttons">
+                <div className="alf-input-column">
+                  <button className={this.handleButton("dog_walks")}
+                    onClick={this.handleButtonClick("dog_walks")}>Free dog walks
+                  </button>
+                  <button className={this.handleButton("deluxe_bed")}
+                    onClick={this.handleButtonClick("deluxe_bed")}>Deluxe doggy bed
+                  </button>
+                  <button className={this.handleButton("house_cat")}
+                    onClick={this.handleButtonClick("house_cat")}>House cat friend
+                  </button>
+                  <button className={this.handleButton("cuddle_buddy")}
+                    onClick={this.handleButtonClick("cuddle_buddy")}>Cuddle buddy</button>
+                </div>
 
-              <span className="alf-field-title">Edit your description</span>
+                <div className="alf-input-column">
+                  <button className={this.handleButton("gourmet_food")}
+                    onClick={this.handleButtonClick("gourmet_food")}>Gourmet dog food
+                  </button>
+                  <button className={this.handleButton("chew_toys")}
+                    onClick={this.handleButtonClick("chew_toys")}>Chew toys</button>
+                  <button className={this.handleButton("frisbee")}
+                    onClick={this.handleButtonClick("frisbee")}>Daily frisbee sessions
+                  </button>
+                  <button className={this.handleButton("mailman")}
+                    onClick={this.handleButtonClick("mailman")}>Mailman visits</button>
+                  <button className={this.handleButton("grooming")}
+                    onClick={this.handleButtonClick("grooming")}>Free grooming</button>
+                </div>
+              </div>
 
-              <span className="alf-field-title alf-header-over">Upload a photo</span>
+              <span className="alf-field-title">Add house rules</span>
 
             </div>
 
             <div className="alf-nav">
-              <Link to="/become-a-host" className="alf-back">Back</Link>
+              <Link to="/become-a-host/basics" className="alf-back">Back</Link>
               <button className={this.handleNext()} onClick={this.handleNextClick}>Next</button>
             </div>
 
