86449c8e10ed09122ccf83468ef1df276ceb989e
Mike Barile <mikebarile13@gmail.com>
2016-11-09 10:02:37 -0800

    Shelled out search page

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
+import ListingItem from './listing_item';
+import Map from './map';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      check_in: null,
+      check_out: null
+    };
+    this.update = this.update.bind(this);
+    this.handleDateType = this.handleDateType.bind(this);
   }
 
   componentWillMount() {
-    this.props.fetchMyListings({"host_id": this.props.currentUser.id});
+
   }
 
   componentDidUpdate() {
@@ -30,32 +38,90 @@ class Search extends React.Component {
       `url('${imageUrl}') center center / cover no-repeat`});
   }
 
+  update(property) {
+    return e => this.setState({
+      [property]: e.target.value
+    });
+  }
+
+  handleDateType(newType, margin) {
+    return (e) => {
+      e.target.type = newType;
+    };
+  }
+
   render() {
 
     return (
       <div className="search">
         <div className="s-form-col">
           <div className="s-form-row">
-
+            <span className="s-dates-title">Dates</span>
+              <input
+                onChange={this.update("check_in")}
+                className="s-date-input"
+                type="text"
+                onFocus={this.handleDateType("date")}
+                placeholder="Check In"/>
+                <input
+                  onChange={this.update("check_out")}
+                  className="s-date-input"
+                  type="text"
+                  onFocus={this.handleDateType("date")}
+                  placeholder="Check In"/>
           </div>
 
           <div className="s-listings">
             <div className="s-listings-row">
-              <div className="s-listing-item">
-                
-              </div>
-
-              <div className="s-listing-item">
-
-              </div>
+              <ListingItem/>
+              <ListingItem/>
             </div>
 
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
+
+            <div className="s-listings-row">
+              <ListingItem/>
+              <ListingItem/>
+            </div>
 
           </div>
         </div>
 
         <div className="s-map-col">
-
+          <Map/>
         </div>
       </div>
     );
