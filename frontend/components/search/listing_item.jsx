229ab347e782b910fb90d55bff39a36b2b744b5b
Mike Barile <mikebarile13@gmail.com>
2016-11-09 15:06:22 -0800

    Added overflow to search page

--- frontend/components/search/listing_item.jsx
+++ frontend/components/search/listing_item.jsx
@@ -10,18 +10,18 @@ class ListingItem extends React.Component {
 
     return (
       <div className="search-listing-item">
-        <img className="home-pick-img" src="https://res.cloudinary.com/dsguwnfdw/image/upload/v1478136940/Spots/under-the-stairs-dog-bed-polished-concrete-floor-built-in-dog-bed.jpg"/>
-        <div className="home-pick-description">
-          <span className="home-pick-price">$294</span>
-          <span className="home-pick-title">Charming post-modern kennel</span>
+        <img className="search-pick-img" src="https://res.cloudinary.com/dsguwnfdw/image/upload/v1478136940/Spots/under-the-stairs-dog-bed-polished-concrete-floor-built-in-dog-bed.jpg"/>
+        <div className="search-pick-description">
+          <span className="search-pick-price">$294</span>
+          <span className="search-pick-title">Charming post-modern kennel</span>
         </div>
-        <div className="home-pick-reviews">
-          <img className="home-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
-          <img className="home-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
-          <img className="home-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
-          <img className="home-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
-          <img className="home-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
-          <span className="home-pick-num-reviews">164 reviews</span>
+        <div className="search-pick-reviews">
+          <img className="search-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
+          <img className="search-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
+          <img className="search-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
+          <img className="search-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
+          <img className="search-pick-star" src="https://res.cloudinary.com/dsguwnfdw/image/upload/c_crop,h_70,w_70,x_3,y_2/v1478139260/Icons/Screen_Shot_2016-11-02_at_7.12.47_PM.png"/>
+          <span className="search-pick-num-reviews">164 reviews</span>
         </div>
       </div>
     );
--- frontend/components/search/map.jsx
+++ frontend/components/search/map.jsx
@@ -11,11 +11,19 @@ class Map extends React.Component {
     const mapDOMNode = this.refs.map;
 
     // set the map to show SF
-    const mapOptions = {
-      center: {lat: parseInt(this.props.lat), lng: parseInt(this.props.lng)}, // this is SF
-      zoom: 13
-    };
-
+    let mapOptions;
+    if(this.props.lat !== "" && this.props.lat !== undefined) {
+      mapOptions = {
+        center: {lat: parseInt(this.props.lat), lng: parseInt(this.props.lng)}, // this is SF
+        zoom: 13
+      };
+    }
+    else {
+      mapOptions = {
+        center: {lat: 0, lng: 0}, // this is SF
+        zoom: 2
+      };
+    }
     // wrap the mapDOMNode in a Google Map
     this.map = new google.maps.Map(mapDOMNode, mapOptions);
   }
