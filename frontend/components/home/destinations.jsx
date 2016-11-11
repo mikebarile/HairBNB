80775dc277854f614a88c2626279881c391c08da
Mike Barile <mikebarile13@gmail.com>
2016-11-11 14:43:50 -0800

    Added seed data factories and seeded database

--- frontend/components/home/destinations.jsx
+++ frontend/components/home/destinations.jsx
@@ -20,24 +20,24 @@ class Destinations extends React.Component {
         <h1 className="home-main-header">Explore the World</h1>
 
         <div className="home-destinations-row">
-          <div className="home-destination-big london" onClick={this.pushDestination('/search?lat=51.5073509&lng=-0.12775829999998223')}>
+          <div className="home-destination-big london" onClick={this.pushDestination('/search?lat=51.5074&lng=-0.1278')}>
             <span className="home-destination-text">London</span>
           </div>
 
           <div className="home-destination-sub-row">
-            <div className="home-destination-small los-angeles" onClick={this.pushDestination('/search?lat=34.0522342&lng=-118.2436849')}>
+            <div className="home-destination-small los-angeles" onClick={this.pushDestination('/search?lat=34.0522&lng=-118.2437')}>
               <span className="home-destination-text">Los Angeles</span>
             </div>
-            <div className="home-destination-small tokyo" onClick={this.pushDestination('/search?lat=35.6894875&lng=139.69170639999993')}>
+            <div className="home-destination-small tokyo" onClick={this.pushDestination('/search?lat=35.6895&lng=139.6917')}>
               <span className="home-destination-text">Tokyo</span>
             </div>
           </div>
 
           <div className="home-destination-sub-row">
-            <div className="home-destination-small washington-dc" onClick={this.pushDestination('/search?lat=38.9071923&lng=-77.03687070000001')}>
+            <div className="home-destination-small washington-dc" onClick={this.pushDestination('/search?lat=38.9072&lng=-77.0369')}>
               <span className="home-destination-text">Washington DC</span>
             </div>
-            <div className="home-destination-small honolulu" onClick={this.pushDestination('/search?lat=21.3069444&lng=-157.85833330000003')}>
+            <div className="home-destination-small honolulu" onClick={this.pushDestination('/search?lat=21.3069&lng=-157.8583')}>
               <span className="home-destination-text">Honolulu</span>
             </div>
           </div>
@@ -45,23 +45,23 @@ class Destinations extends React.Component {
 
         <div className="home-destinations-row">
           <div className="home-destination-sub-row">
-            <div className="home-destination-small paris" onClick={this.pushDestination('/search?lat=48.856614&lng=2.3522219000000177')}>
+            <div className="home-destination-small paris" onClick={this.pushDestination('/search?lat=48.8566&lng=2.3522')}>
               <span className="home-destination-text">Paris</span>
             </div>
-            <div className="home-destination-small berlin" onClick={this.pushDestination('/search?lat=52.52000659999999&lng=13.404953999999975')}>
+            <div className="home-destination-small berlin" onClick={this.pushDestination('/search?lat=52.5200&lng=13.4050')}>
               <span className="home-destination-text">Berlin</span>
             </div>
           </div>
 
-          <div className="home-destination-big san-francisco" onClick={this.pushDestination('/search?lat=37.7749295&lng=-122.41941550000001')}>
+          <div className="home-destination-big san-francisco" onClick={this.pushDestination('/search?lat=37.7749&lng=-122.4194')}>
             <span className="home-destination-text">San Francisco</span>
           </div>
 
           <div className="home-destination-sub-row">
-            <div className="home-destination-small cairo" onClick={this.pushDestination('/search?lat=30.1197986&lng=31.537000300000045')}>
+            <div className="home-destination-small cairo" onClick={this.pushDestination('/search?lat=30.0444&lng=13.4050')}>
               <span className="home-destination-text">Cairo</span>
             </div>
-            <div className="home-destination-small new-york" onClick={this.pushDestination('/search?lat=40.7127837&lng=-74.00594130000002')}>
+            <div className="home-destination-small new-york" onClick={this.pushDestination('/search?lat=40.7128&lng=-74.0059')}>
               <span className="home-destination-text">New York</span>
             </div>
           </div>
